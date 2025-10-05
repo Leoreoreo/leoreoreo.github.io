ac86c32567126f19ef35be4ff51e099bf9d5d5df
LeyangLi <87118867+Leoreoreo@users.noreply.github.com>
2025-10-05 02:38:25 -0400

    try to deploy

--- astro.config.ts
+++ astro.config.ts
@@ -29,12 +29,13 @@ export default defineConfig({
   // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
   trailingSlash: 'never',
+  output: 'static',
 
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
-  output: 'server',
+  // adapter: vercel(),
+  // output: 'server',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
   // 3. Local (standalone)
